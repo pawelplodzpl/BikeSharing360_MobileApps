42cb6518bfe2ebce15bc113f72d3b56f8a841726
Pavel K <drpavel@hotmail.com>
2017-05-10 09:55:17 +0200

    Repairing missing nuGet packages

--- src/BikeSharing.Clients.Core/GlobalSettings.cs
+++ src/BikeSharing.Clients.Core/GlobalSettings.cs
@@ -4,19 +4,19 @@ namespace BikeSharing.Clients.Core
 {
     public static class GlobalSettings
     {
-        public const string AuthenticationEndpoint = "http://YOUR_PROFILE_SERVICE.azurewebsites.net/";
-        public const string EventsEndpoint = "http://YOUR_EVENTS_SERVICE.azurewebsites.net/";
-        public const string IssuesEndpoint = "http://YOUR_ISSUES_SERVICE.azurewebsites.net/";
-        public const string RidesEndpoint = "http://YOUR_RIDES_SERVICE.azurewebsites.net/";
+        public const string AuthenticationEndpoint = "http://bikesharing-services-profilesg2m7lgvod5pme.azurewebsites.net";
+        public const string EventsEndpoint = "http://bikesharing-services-eventsg2m7lgvod5pme.azurewebsites.net/";
+        public const string IssuesEndpoint = "http://bikesharing-services-feedbackg2m7lgvod5pme.azurewebsites.net";
+        public const string RidesEndpoint = "http://bikesharing-services-ridesg2m7lgvod5pme.azurewebsites.net";
 
-        public const string OpenWeatherMapAPIKey = "YOUR_WEATHERMAP_API_KEY";
+        public const string OpenWeatherMapAPIKey = "f90487349bfa6f5a954fa964f2737b0f";
 
         public const string HockeyAppAPIKeyForAndroid = "YOUR_HOCKEY_APP_ID";
         public const string HockeyAppAPIKeyForiOS = "YOUR_HOCKEY_APP_ID";
 
         public const string SkypeBotAccount = "skype:YOUR_BOT_ID?chat";
 
-        public const string BingMapsAPIKey = "YOUR_BINGMAPS_API_KEY";
+        public const string BingMapsAPIKey = "ArDvHkD19Q8zMT15x0CeQXLaOzCUcqlzf30qc5PGpSB_w1m9Pm6QTDb9Cr_VQywy";
 
 
         public static string City => "Redmond";
